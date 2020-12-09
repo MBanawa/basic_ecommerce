d8ad447bed324daf885e940ca4e26dfef9264ca2
MBanawa <mikebanawa25@gmail.com>
2020-12-10 03:03:51 +0800

    added user auth error handling

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:basic_ecommerce_app/models/http_exception.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,17 +12,24 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCXZVfoZUouBlzuYYbf8omv3zRSwIgmLK8';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
